3102e0a30b0a2cc3d77ca969aad2494a18603f4f
Wahidur Rahman <wrrahman456@gmail.com>
2024-07-10 08:51:27 +0600

    Even Odd With String

---
+++ Even Odd With String.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    gets(str);
+    int i=strlen(str)-1;
+    if(str[i] % 2 == 0)
+    {
+        printf("The number is even");
+    }
+    else {
+        printf("The number is odd");
+    }
+
+    return 0;
+}
