32d16ca8523e0bb8848a808fbac42ca555593313
Wahidur Rahman <wrrahman456@gmail.com>
2024-07-11 12:47:22 +0600

    Sorting

--- Sorting.c
+++ Sorting.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 int main()
 {
-    int n, A[1006], i, j, x;
+    int n, arr[1006], i, j, x;
     scanf("%d", &n);
     for(i=0;i<n;i++)
     {
-        scanf("%d", &A[i]);
+        scanf("%d", &arr[i]);
     }
     for(i=0;i<n-1;i++)
     {
         x = i;
         for(j=i+1;j<n;j++)
         {
-            if (A[j] < A[x])
+            if (arr[j] < arr[x])
             {
                 x = j;
             }
         }
-        int temp = A[i];
-        A[i] = A[x];
-        A[x] = temp;
+        int temp = arr[i];
+        arr[i] = arr[x];
+        arr[x] = temp;
     }
     for(i=0;i<n;i++)
     {
-        printf("%d ", A[i]);
+        printf("%d ", arr[i]);
     }
  
     return 0;
