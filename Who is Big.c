d8ad06980e9b4784f6a0412e67793765e07ab398
Wahidur Rahman <wrrahman456@gmail.com>
2024-07-18 10:15:14 +0600

    Who is Big

---
+++ Who is Big.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int number_of_test_case, i;
+    char longest_str[10000] = "";
+    char str[10000];
+
+    scanf("%d", &number_of_test_case);
+    getchar();
+
+    for (i = 0; i < number_of_test_case; i++)
+    {
+        gets(str);
+        if (strlen(str) > strlen(longest_str))
+        {
+            strcpy(longest_str, str);
+        }
+    }
+    puts(longest_str);
+
+    return 0;
+}
